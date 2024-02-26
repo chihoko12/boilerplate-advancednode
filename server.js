e2b39b36267cbcf404ae20ee8f07143ea3a07fcf
Chihoko Neudeck <chihoko.ueno@gmail.com>
2024-02-26 22:31:17 +0000

    set up a template engine

--- server.js
+++ server.js
@@ -3,8 +3,12 @@ require('dotenv').config();
 const express = require('express');
 const myDB = require('./connection');
 const fccTesting = require('./freeCodeCamp/fcctesting.js');
+const res = require('express/lib/response.js');
 
 const app = express();
+
+app.set('view engine', 'pug'); 
+app.set('views', './views/pug');
 
 fccTesting(app); //For FCC testing purposes
 app.use('/public', express.static(process.cwd() + '/public'));
@@ -12,7 +16,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.route('/').get((req, res) => {
-
+  res.render('index'); 
 });
 
 const PORT = process.env.PORT || 3000;
