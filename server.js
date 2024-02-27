c35cbfeaf5f5501c896a59d298ce7a1afd534ec1
Chihoko Neudeck <chihoko.ueno@gmail.com>
2024-02-27 19:24:11 +0000

    setup passport

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const express = require('express');
 const myDB = require('./connection');
 const fccTesting = require('./freeCodeCamp/fcctesting.js');
 const res = require('express/lib/response.js');
+const session = require('express-session');
+const passport = require('passport');
 
 const app = express();
 
@@ -14,6 +16,16 @@ fccTesting(app); //For FCC testing purposes
 app.use('/public', express.static(process.cwd() + '/public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: true,
+  saveUninitialized: true,
+  cookie: { secure: false }
+}));
+
+passport.initialize();
+passport.session();
 
 app.route('/').get((req, res) => {
   res.render('index', { title:'Hello', message: 'Please log in' }); 
