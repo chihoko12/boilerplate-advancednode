3902a4e0eb772b6fc1b7b47db7f930b12c576847
cu <harrierraum@hotmail.com>
2024-02-29 22:49:53 +0100

    hash password

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ const session = require('express-session');
 const passport = require('passport');
 const { ObjectID } = require('mongodb');
 const LocalStrategy = require('passport-local');
+const bcrypt = require('bcrypt');
 
 const app = express();
 
@@ -58,6 +59,7 @@ myDB(async client=> {
 
   app.route('/register')
     .post((req, res, next) => {
+      const hash = bcrypt.hashSync(req.body.password, 12);
       myDataBase.findOne({ username: req.body.username }, (err, user) => {
         if (err) {
           next(err);
@@ -66,7 +68,7 @@ myDB(async client=> {
         } else {
           myDataBase.insertOne({
             username: req.body.username,
-            password: req.body.password
+            password: hash
           },
             (err, doc) => {
               if (err) {
@@ -96,7 +98,7 @@ myDB(async client=> {
       console.log(`User ${username} attempted to log in.`);
       if (err) return done(err);
       if (!user) return done(null, false);
-      if (password != user.password) return done(null, false);
+      if (!bcrypt.compareSync(password, user.password)) return done(null, false);
       return done(null, user);
     });
   }));
