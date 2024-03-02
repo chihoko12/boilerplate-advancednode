941d38c7caaffddac635d15f78f6bc9e00cdd50c
cu <harrierraum@hotmail.com>
2024-03-02 10:22:57 +0100

    authentication with socket.io

--- server.js
+++ server.js
@@ -12,6 +12,11 @@ const app = express();
 
 const http = require('http').createServer(app);
 const io = require('socket.io')(http);
+const passportSocketIo = require('passport.socketio');
+const cookieParser = require('cookie-parser');
+const MongoStore = require('connect-mongo')(session);
+const URI = process.env.MONGO_URI;
+const store = new MongoStore({ url: URI });
 
 app.set('view engine', 'pug');
 app.set('views', './views/pug');
@@ -30,6 +35,17 @@ fccTesting(app); //For FCC testing purposes
 app.use('/public', express.static(process.cwd() + '/public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+io.use(
+  passportSocketIo.authorize({
+    cookieParser: cookieParser,
+    key: 'express.sid',
+    secret: process.env.SESSION_SECRET,
+    store: store,
+    success: onAuthorizeSuccess,
+    fail: onAuthorizeFail
+  })
+);
 
 myDB(async client=> {
   const myDataBase = await client.db('database').collection('users');
@@ -55,6 +71,19 @@ myDB(async client=> {
     res.render('index', { title: e, message: 'Unable to connect to databse'});
   });
 });
+
+function onAuthorizeSuccess(data, message, error, accept) {
+  console.log('successful connection to socket.io');
+  accept(null, true);
+}
+
+function onAuthorizeFail(data, message, error, accept) {
+  if (error) throw new Error(message);
+  console.log('failed connection to socket.io:', message);
+  accept(null, false);
+}
+
+
 
 const PORT = process.env.PORT || 3000;
 http.listen(PORT, () => {
